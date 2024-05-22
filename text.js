50aaa5d71b363280d9c984ffbb6c72120cd66203
IStasovskyi93 <stsovsky400m@gmail.com>
2024-05-22 13:04:11 +0200

    changes names

--- text.js
+++ text.js
@@ -1,4 +1,12 @@
 const myName = "Ihor";
 console.log(myName);
-myName = "Iryna";
-console.log(myName);
+const objectA = {
+  a: 10,
+  b: 5,
+};
+console.log(objectA.b);
+const objectCopy = objectA;
+objectCopy.b = 20;
+console.log(objectA.b, objectCopy.b);
+objectA.c = 30;
+console.table(objectA);
