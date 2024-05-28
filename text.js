2c0e8d7df84f1506b00fb3eb8b6a17db9a651f7c
IStasovskyi93 <stsovsky400m@gmail.com>
2024-05-28 17:32:46 +0200

    function prompt

--- text.js
+++ text.js
@@ -1,12 +1,1 @@
-const myName = "Ihor";
-console.log(myName);
-const objectA = {
-  a: 10,
-  b: 5,
-};
-console.log(objectA.b);
-const objectCopy = objectA;
-objectCopy.b = 20;
-console.log(objectA.b, objectCopy.b);
-objectA.c = 30;
-console.table(objectA);
+prompt("Who are you?");
