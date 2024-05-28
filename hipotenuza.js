798cdd3d521be72f8d20ea38ed6cb3c85b878d11
IStasovskyi93 <stsovsky400m@gmail.com>
2024-05-28 20:40:20 +0200

    create calculator of hypotenuze

---
+++ hipotenuza.js
@@ -0,0 +1,5 @@
+let line1 = prompt("Input length of katet1: ");
+let line2 = prompt("Input length of katet2: ");
+let hypotenuza = (line1 ** 2 + line2 ** 2) * 0.5;
+//console.log(hypotenuza);
+alert(hypotenuza);
--- text.js
+++ text.js
@@ -2,3 +2,13 @@
 prompt("Who are you!");
 console.log("Me ciamo Igor");
 console.log(Math.floor(Math.random() * 100));
+let zeroToBool = null;
+zeroToBool = Boolean(zeroToBool);
+console.log(zeroToBool, typeof zeroToBool);
+let name = "Igor";
+let age = 31;
+let Can_JS = "yes";
+let answer = Boolean(Can_JS);
+console.log(
+  `My name is ${name}, I\`ve ${age} old, and I coding on JS - it\`s ${answer}`
+);
