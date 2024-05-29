c583c80d973a24ac05fec2dc6f5cdd85fc803009
IStasovskyi93 <stsovsky400m@gmail.com>
2024-05-29 19:04:04 +0200

    create calculator mils

--- text.js
+++ text.js
@@ -1,14 +1,3 @@
-//alert("Don`t fright)");
-prompt("Who are you!");
-console.log("Me ciamo Igor");
-console.log(Math.floor(Math.random() * 100));
-let zeroToBool = null;
-zeroToBool = Boolean(zeroToBool);
-console.log(zeroToBool, typeof zeroToBool);
-let name = "Igor";
-let age = 31;
-let Can_JS = "yes";
-let answer = Boolean(Can_JS);
-console.log(
-  `My name is ${name}, I\`ve ${age} old, and I coding on JS - it\`s ${answer}`
-);
+let km = 1;
+let mil = km * 1.609344;
+console.log(`Distantance 130 mil equal ${130 * mil} km`);
