eccbaa5784a1b6e8225ed0d595e5bdd2c03de1b1
IStasovskyi93 <stsovsky400m@gmail.com>
2024-05-30 13:23:44 +0200

    create quetion and allowed

--- text.js
+++ text.js
@@ -1,6 +1,10 @@
-let people = { friends: [] };
-let Vova = { name: "Vova", lastName: "Erpa", ID: 3456 };
-let Sanya = { name: "Sanya", lastName: "Polo", ID: 7689 };
-let Denya = { name: "Denya", lastName: "Volo", ID: 7893 };
-people.friends.push(Vova, Sanya, Denya);
-console.table(people.friends);
+let age = Number(prompt("How old are you?"));
+let message;
+if (age >= 21) {
+  message = "You can to enter and to buy alcohol";
+} else if (age >= 19) {
+  message = "You can to enter entire but can`t to buy alcohol";
+} else {
+  message = "You can`t to enter";
+}
+console.log(message);
