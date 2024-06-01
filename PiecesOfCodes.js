f6b141654a160d2cc6d1b0bffab7d01e6cf701cd
IStasovskyi93 <stsovsky400m@gmail.com>
2024-06-01 15:19:08 +0200

    create array with cycleInCycle

--- PiecesOfCodes.js
+++ PiecesOfCodes.js
@@ -9,10 +9,23 @@ do {
 } while (condition != 100);
 console.log(score);
 
-/* Cycle for*/
+/* Cycle for. 
+I get array with 10 objects {name: lesson(DIGIT), status true or false(it`s depends of DIGIT)}*/
 let myWork = [];
-digit = 0;
-for (let lesson = []; lesson.length < 10; digit++) {
-  lesson.push({ name: "lesson" + digit, status: digit % 2 ? true : false });
+for (let digit = 0; myWork.length < 10; digit++) {
+  let lesson = { name: "lesson" + digit, status: digit % 2 ? true : false };
+  myWork.push(lesson);
 }
-console.table(lesson);
+console.log(myWork, myWork.length);
+
+/* Cycle in cycle.
+Create table 5 rows/7 columns with help cycle in cycle*/
+let myTable = [];
+let rows = 5;
+let columns = 7;
+for (let counter = 0; myTable.length < rows; counter++) {
+  myTable.push([]);
+  for (let template = 0; myTable[counter].length < columns; template++)
+    myTable[counter].push(template);
+}
+console.table(myTable);
--- text.js
+++
@@ -1,9 +0,0 @@
-/* Cycle for. 
-I get array with 10 objects {name: lesson(DIGIT), status true or false(it`s depends of DIGIT)}*/
-
-let myWork = [];
-for (let digit = 0; myWork.length < 10; digit++) {
-  let lesson = { name: "lesson" + digit, status: digit % 2 ? true : false };
-  myWork.push(lesson);
-}
-console.log(myWork, myWork.length);
