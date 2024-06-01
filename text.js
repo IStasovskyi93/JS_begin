b89a267b5569b2275f4f152d6aedd9110f460b26
IStasovskyi93 <stsovsky400m@gmail.com>
2024-06-01 22:38:32 +0200

    start function lerning

--- text.js
+++ text.js
@@ -1,9 +1,2 @@
-let poem = "Gay is russian poet";
-let forget = "is";
-for (let counter = 0; counter < 10; counter++) {
-  if (poem[counter] + poem[counter + 1] === "is") {
-    continue;
-  }
-  poem = poem + poem[counter] + poem[counter + 2] + " ";
-}
-console.log(poem);
+let ask = prompt("Input digit");
+console.log(ask);
