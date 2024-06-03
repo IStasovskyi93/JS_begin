72719eda38842153f7b195b088cdc7531e1048ec
IStasovskyi93 <stsovsky400m@gmail.com>
2024-06-03 11:30:31 +0200

    lerning IIFE

--- text.js
+++ text.js
@@ -1,3 +1,8 @@
-let ask = prompt("Input digit");
-console.log(ask);
-nsdjkfnsdk = "sbfshbfgrk";
+/*Immediatly involved function expression*/
+let number = 1000;
+let result = (function () {
+  let number = 500;
+  console.log(number);
+})();
+//let result = 200;
+console.log(result);
