7f97fc3cd394f144331c253e0c9da6a480125181
IStasovskyi93 <stsovsky400m@gmail.com>
2024-06-04 11:03:54 +0200

    lerning functions JS

--- text.js
+++ text.js
@@ -1,23 +1,7 @@
-/*Recoursive function.
-Calculation factorial*/
-
-function factorial(digit) {
-  if (digit === 0) {
-    return 1;
-  } else {
-    let digits = [];
-    for (digit; digit >= 2; digit -= 1) {
-      digits.push(digit);
-    }
-    for (let item in digits) {
-      let result = [];
-      while (item < digits.length) {
-        result.push(item + "");
-      }
-      result.push(item);
-      item *= item;
-    }
-    return result + "=" + item;
+function counter(digit) {
+  while (digit <= 10) {
+    console.log(digit);
+    digit += 1;
   }
 }
-factorial(8);
+counter(4);
