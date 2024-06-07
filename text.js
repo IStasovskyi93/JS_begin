e43f9461035f5b1b958050b82c230556a2fa01e0
IStasovskyi93 <stsovsky400m@gmail.com>
2024-06-07 09:01:52 +0200

    create class Employee

--- text.js
+++ text.js
@@ -1,16 +1,51 @@
-class Person {
-  constructor(firstname, lastname) {
-    this.firstname = firstname;
-    this.lastname = lastname;
+//Create class employee with getters and setters
+
+class Employee {
+  #expirience;
+  #firstname;
+  #lastname;
+  constructor(firstname, lastname, expirience) {
+    this.#firstname = firstname;
+    this.#lastname = lastname;
+    this.expirience = expirience;
   }
-  fullName() {
-    return this.firstname + " " + this.lastname;
+  get firstname() {
+    return this.#firstname;
   }
+  set firstname(firstname) {
+    this.#firstname = firstname;
+  }
+  get lastname() {
+    return this.#lastname;
+  }
+  set lastname(lastname) {
+    this.#lastname = lastname;
+  }
+  get expirience() {
+    return this.#expirience;
+  }
+  set expirience(expirience) {
+    this.#expirience = expirience;
+  }
+  // infoPerson = function () {
+  //   return this.firstname + this.lastname + "have expirience about"
+  //   + this.expirience +"years."
+  // }
 }
 
-let vova = new Person("Vova", "Erpelov");
-let sanya = new Person("Sasha", "Polozhai");
-let person1 = vova.fullName();
-let person2 = sanya.fullName();
-console.log(`${person1} and ${person2}`);
-console.log(`${vova.fullName()} and ${sanya.fullName()}`);
+/*Create objects employees and add function through prototype which 
+watch info about employees*/
+let person1 = new Employee("Ivan", "Karas", 8);
+let person2 = new Employee("Oksana", "Bond", 2);
+let employees = [person1, person2];
+Employee.prototype.infoPerson = function () {
+  return (
+    this.firstname +
+    " " +
+    this.lastname +
+    " have expirience about " +
+    this.expirience +
+    " years."
+  );
+};
+employees.forEach((person) => console.log(person.infoPerson()));
