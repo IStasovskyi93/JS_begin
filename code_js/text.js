274c83376bd13d2d8845fb0e44fe5b869162909c
IStasovskyi93 <stsovsky400m@gmail.com>
2024-06-14 21:01:10 +0200

    lessons about DOM

--- code_js/text.js
+++ code_js/text.js
@@ -1,3 +1,9 @@
 "use strict";
-let gratulation = document.querySelector("p.output");
-console.dir(gratulation);
+const gratulation = document.querySelector(".output");
+const bodyElement = document.body;
+const childNodes = bodyElement.childNodes;
+console.log(childNodes);
+const childrenNodes = bodyElement.children;
+const imageBird = document.querySelector("img");
+console.log(childrenNodes);
+console.log(gratulation, imageBird);
