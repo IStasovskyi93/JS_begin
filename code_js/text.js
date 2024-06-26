11e9791a4a16fd3cc0e55228e66f5dae6d151d6a
IStasovskyi93 <stsovsky400m@gmail.com>
2024-06-26 22:41:47 +0200

    add random gradient background to body

--- code_js/text.js
+++ code_js/text.js
@@ -1,35 +1,31 @@
-/*document.querySelector(".output").addEventListener("click", changeEl);
+let arrNames = ["Ivan", "Antonij", "Igor", "Iryna", "Helena", "Walerij"];
+document.querySelector(".output").addEventListener("click", changeEl);
 
 let doIt = document.getElementsByClassName("doSomething");
 doIt[0].style.color = "green";
 
 function changeEl() {
   this.style.color = "blue";
-}*/
-function randomNumber(number) {
-  return Math.floor(Math.random() * (number + 1));
 }
 let push = document.getElementById("push");
 push.addEventListener("click", changeColor);
 function changeColor() {
-  let draw = `rgb(${randomNumber(255)}, ${randomNumber(255)}, ${randomNumber(
+  function randomNumber(number) {
+    return Math.floor(Math.random() * (number + 1));
+  }
+  let draw = `linear-gradient(90deg, rgba(${randomNumber(255)}, ${randomNumber(
     255
-  )}})`;
-  document.body.style.backgroundColor = draw;
+  )}, ${randomNumber(255)}, 1) 0%, 
+rgba(${randomNumber(255)}, ${randomNumber(255)}, ${randomNumber(255)}, 1) 0%, 
+rgba(${randomNumber(255)}, ${randomNumber(255)}, ${randomNumber(255)}, 1) 50%, 
+rgba(${randomNumber(255)}, ${randomNumber(255)}, ${randomNumber(
+    255
+  )}, 1) 100%)`;
+  document.body.style.background = draw;
 }
-push.style.backgroundColor = "red";
 
 let bird = document.querySelectorAll("li");
-bird.forEach((item) => console.log(item));
-console.log(bird);
-/*const btn = document.getElementById("push");
 
-function random(number) {
-  return Math.floor(Math.random() * (number + 1));
-}
+let names = document.querySelector("div.nameSpaces");
 
-btn.onclick = function () {
-  const rndCol =
-    "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
-  document.body.style.backgroundColor = rndCol;
-};*/
+/*`rgb(${randomNumber(255)}, ${randomNumber(255)}, ${randomNumber(255)})`;*/
